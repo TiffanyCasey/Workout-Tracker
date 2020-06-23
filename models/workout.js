b5512096080a1056943298021907b5c16b1896be
“TiffanyCasey” <tiffanylynnecasey@gmail.com>
2020-06-23 11:01:38 -0400

    updated schema

--- models/workout.js
+++ models/workout.js
@@ -3,7 +3,41 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  name: {
+    type: String,
+    trim: true,
+    required: "Excercise Type"
+  },
+  name: {
+    type: String,
+    trim: true,
+    required: "Excercise Name"
+  },
+  value: {
+    type: Number,
+    required: "Distance"
+  },
+  value: {
+    type: Number,
+    required: "Duration"
+  },
 
+  value: {
+    type: Number,
+    required: "Weight"
+  },
+  value: {
+    type: Number,
+    required: "Sets"
+  },
+  value: {
+    type: Number,
+    required: "Reps"
+  },
   });
   
   const Workout = mongoose.model("Transaction", workoutSchema);
