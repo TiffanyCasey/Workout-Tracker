ca369d180aa551096b6ed0cb6255c9da559bb5dd
“TiffanyCasey” <tiffanylynnecasey@gmail.com>
2020-07-06 16:21:47 -0400

    fixed routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,6 @@
 const app = require("express").Router();
 const Workout = require("../models/workouts.js");
 
- // Read last workout - DONE 
-app.get("/api/workouts", (req, res) => {
-  console.log("Reading workout")
-  Workout.find({})
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
-
 // Create a new workout DONE 
 app.post("/api/workouts", ({ body }, res) => {
   console.log("Creating a new workout")
@@ -25,18 +13,25 @@ app.post("/api/workouts", ({ body }, res) => {
     });
 });
 
+ // Read last workout - DONE 
+ app.get("/api/workouts", (req, res) => {
+  console.log("Reading workout")
+  Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
 // Add an exercise - DONE
-app.put("/api/workouts/:id", ({ params }, res) => {
+app.put("/api/workouts/:id", (req, res) => {
   console.log("Adding an exercise")
-  Workout.update(
-    {
-      _id: mongojs.ObjectId(params.id)
-    },
-    {
-      $push: {
-        exercises: body
-      }
-    },
+  Workout.findByIdAndUpdate(
+    req.params.id,
+    { $push: {exercises: req.body}},
+    { new: true}
   )
     .then(dbWorkout => {
       res.json(dbWorkout);
@@ -47,7 +42,9 @@ app.put("/api/workouts/:id", ({ params }, res) => {
 });
 
 
-// Gets daily workouts and aggregates into charts. Not clear how this is differnt from app.post("/api/workouts"
+
+
+// Gets daily workouts and aggregates into charts. Not clear how this is different from app.post("/api/workouts"
 app.get("/api/workouts/range", (req, res) => {
   Workout.find({})
   .then(dbWorkout => {
