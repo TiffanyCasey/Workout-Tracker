cf94de783aac3d9f7d2fb0324f65621af638ec73
“TiffanyCasey” <tiffanylynnecasey@gmail.com>
2020-07-06 16:43:42 -0400

    added heroku connection

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // db Mongo
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://workouttracker:workouttracker1>@ds127439.mlab.com:27439/heroku_r65p84lc", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
